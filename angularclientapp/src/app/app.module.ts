d4bbdb3be7c1aec238d7ad1be4cefa7d5b3b20cd
Shahed1998 <shahedchowdhury120@gmail.com>
2023-01-14 23:09:11 +0600

    property, and navbar component added

--- angularclientapp/src/app/app.module.ts
+++ angularclientapp/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PropertyCardComponent, PropertyListComponent, NavbarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
