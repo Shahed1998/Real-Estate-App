c35e540a1f36fcefba7497da56993c5b52d39ba0
Shahed1998 <shahedchowdhury120@gmail.com>
2023-01-27 19:20:12 +0600

    added dtos, controllers, services, repos

--- server/DAL/DataAccessFactory.cs
+++ server/DAL/DataAccessFactory.cs
@@ -1,7 +1,14 @@
-﻿namespace DAL
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using DAL.Repos;
+
+namespace DAL
 {
     public class DataAccessFactory
     {
-
+        public static IRepo<Property> PropertyDataAccess()
+        {
+            return new PropertyRepo();
+        }
     }
 }
---
+++ server/DAL/Repos/PropertyRepo.cs
@@ -0,0 +1,38 @@
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class PropertyRepo : IRepo<Property>
+    {
+        public Property Add(Property obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Property> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Property Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Property Update(Property obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
