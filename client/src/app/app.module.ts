f3e9c35d5ab5338bf737399c85a8b3711e28aaf5
Shahed1998 <shahedchowdhury120@gmail.com>
2023-02-04 13:32:57 +0600

    ...

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+
 import { PropertyCardComponent } from './components/property/property-card/property-card.component';
 import { PropertyListComponent } from './components/property/property-list/property-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ApiService } from './services/api.service';
+import { AddPropertyComponent } from './components/property/add-property/add-property.component';
+import { PropertyDetailComponent } from './components/property/property-detail/property-detail.component';
+
+const appRoutes: Routes = [
+  { path: '', component: PropertyListComponent },
+  { path: 'rent-property', component: PropertyListComponent },
+  { path: 'add-property', component: AddPropertyComponent },
+  { path: 'property-detail/:id', component: PropertyDetailComponent },
+];
 
 @NgModule({
   declarations: [
@@ -13,8 +24,10 @@ import { ApiService } from './services/api.service';
     PropertyCardComponent,
     PropertyListComponent,
     NavbarComponent,
+    AddPropertyComponent,
+    PropertyDetailComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
   providers: [ApiService],
   bootstrap: [AppComponent],
 })
