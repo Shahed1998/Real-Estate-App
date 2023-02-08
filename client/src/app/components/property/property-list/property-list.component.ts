e4ebf045f8e11d4e4affe8b9d037c8900f118e7c
Shahed1998 <shahedchowdhury120@gmail.com>
2023-02-08 10:36:24 +0600

    ...

--- client/src/app/components/property/property-list/property-list.component.ts
+++ client/src/app/components/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { IProperty } from '../Interfaces/iproperty';
 
@@ -9,13 +10,18 @@ import { IProperty } from '../Interfaces/iproperty';
 })
 export class PropertyListComponent implements OnInit {
   properties!: Array<IProperty>;
+  SellRent: number = 1;
 
-  constructor(private apiservice: ApiService) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private apiservice: ApiService
+  ) {}
 
   ngOnInit(): void {
     this.apiservice.getAllProperties().subscribe(
       (res) => {
         this.properties = res;
+        console.log(this.activatedRoute.snapshot.url.toString());
       },
       (err) => {
         console.log(err);
