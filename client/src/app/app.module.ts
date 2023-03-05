c5dd605cc8cd62e30eea2e2a59a9338d4e52afc0
Shahed1998 <shahedchowdhury120@gmail.com>
2023-03-06 01:23:44 +0600

    reactive forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PropertyCardComponent } from './components/property/property-card/property-card.component';
 import { PropertyListComponent } from './components/property/property-list/property-list.component';
@@ -13,12 +13,16 @@ import { ApiService } from './services/api.service';
 import { AddPropertyComponent } from './components/property/add-property/add-property.component';
 import { PropertyDetailComponent } from './components/property/property-detail/property-detail.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { UserLoginComponent } from './components/user/user-login/user-login.component';
+import { UserRegisterComponent } from './components/user/user-register/user-register.component';
 
 const appRoutes: Routes = [
   { path: '', component: PropertyListComponent },
   { path: 'rent-property', component: PropertyListComponent },
   { path: 'add-property', component: AddPropertyComponent },
   { path: 'property-detail/:id', component: PropertyDetailComponent },
+  { path: 'user/login', component: UserLoginComponent },
+  { path: 'user/register', component: UserRegisterComponent },
   { path: '**', component: PageNotFoundComponent, data: { navbar: false } },
 ];
 
@@ -31,6 +35,8 @@ const appRoutes: Routes = [
     AddPropertyComponent,
     PropertyDetailComponent,
     PageNotFoundComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
   ],
   imports: [
     BrowserModule,
@@ -38,6 +44,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     LazyLoadImageModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [ApiService],
   bootstrap: [AppComponent],
