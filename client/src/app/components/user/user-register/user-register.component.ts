082b9679088b3f75f7360e94e8636428fb2f03d7
Shahed1998 <shahedchowdhury120@gmail.com>
2023-03-08 18:52:17 +0600

    need fix: custom validation to registration form

--- client/src/app/components/user/user-register/user-register.component.ts
+++ client/src/app/components/user/user-register/user-register.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.css'],
 })
-export class UserRegisterComponent {
+export class UserRegisterComponent implements OnInit {
   registrationForm!: FormGroup;
+  ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      confirmPassword: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      phoneNumber: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(11),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm);
+  }
 }
