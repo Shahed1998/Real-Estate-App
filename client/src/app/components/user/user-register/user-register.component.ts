f9b12f7597b749d2b4feaf735a521dfa68fff975
Shahed1998 <shahedchowdhury120@gmail.com>
2023-03-10 21:23:39 +0600

    ...

--- client/src/app/components/user/user-register/user-register.component.ts
+++ client/src/app/components/user/user-register/user-register.component.ts
@@ -16,22 +16,34 @@ import {
 export class UserRegisterComponent implements OnInit {
   registrationForm!: FormGroup;
   ngOnInit(): void {
-    this.registrationForm = new FormGroup({
-      name: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(8),
-      ]),
-      confirmPassword: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(8),
-      ]),
-      phoneNumber: new FormControl(null, [
-        Validators.required,
-        Validators.maxLength(11),
-      ]),
-    });
+    this.registrationForm = new FormGroup(
+      {
+        name: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(8),
+        ]),
+        confirmPassword: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(8),
+        ]),
+        phoneNumber: new FormControl(null, [
+          Validators.required,
+          Validators.maxLength(11),
+        ]),
+      },
+      // Custom validation
+      { validators: [this.confirmPasswordValidationFn] }
+    );
+  }
+
+  // Custom validation function
+  confirmPasswordValidationFn(control: AbstractControl) {
+    return control.get('password')?.value ===
+      control.get('confirmPassword')?.value
+      ? null
+      : { notmatched: true };
   }
 
   onSubmit() {
