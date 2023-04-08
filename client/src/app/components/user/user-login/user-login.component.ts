a4fb9a6cfc4dbb7df8d80bac51760fd3411b5fa9
Shahed1998 <shahedchowdhury120@gmail.com>
2023-04-08 14:44:32 +0600

    added validations to login form

--- client/src/app/components/user/user-login/user-login.component.ts
+++ client/src/app/components/user/user-login/user-login.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.css'],
 })
-export class UserLoginComponent {
+export class UserLoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginFormBuilder();
+  }
+
+  // -------------------------------------------------------------------------
+  // Form builder
+  // --------------------------------------------------------------------------
+
+  loginFormBuilder() {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  // -------------------------------------------------------------------------
+  // Getter method for all form controls
+  // --------------------------------------------------------------------------
+
+  get Email() {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get Password() {
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  login() {
+    console.log(this.loginForm);
+  }
 }
