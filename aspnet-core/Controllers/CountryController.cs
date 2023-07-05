1cd28e70bf1253d630954d908ab70257e6bc1853
Shahed1998 <shahedchowdhury120@gmail.com>
2023-07-06 02:03:44 +0600

    initial backend: added models and dtos

---
+++ aspnet-core/Controllers/CountryController.cs
@@ -0,0 +1,31 @@
+﻿using aspnet_core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetList()
+        {
+            
+            List<Country> countryList = new List<Country>();
+
+            countryList.Add(
+                new Country 
+                { 
+                    Id=1,
+                    Name="Bangladesh",
+                    InternetCountryCode= "BD",
+                    CountryCallingCode= "+880",
+                    Nationality="Bangladeshi",
+                    Flag= "assets/images/countries-flag/bd.flag.png"
+                });
+
+            return Ok(countryList);
+        }
+    }
+}
---
+++ aspnet-core/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace aspnet_core.Models
+{
+    public class Country
+    {
+        public int Id { get; set; } = 0;
+        public string? Name { get; set; }
+        public string? InternetCountryCode { get; set; }
+        public string? CountryCallingCode { get; set; }
+        public string? Nationality { get; set; }
+        public string? Flag { get; set; }
+    }
+}
